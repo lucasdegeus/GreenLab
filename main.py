557de7abca996854ea040167083e6bc94fcd7988
ReiniervdG <reinier@vandergronden.com>
2021-09-30 10:26:35 +0200

    Update repo setup

---
+++ main.py
@@ -0,0 +1,7 @@
+# Let's see if this is possible:
+
+# Checks if some substep of data exists, otherwise runs Prep 01, 02, 03 javascript?
+
+# Starts Flask server
+
+# Executes command line argument for starting androidRunner
