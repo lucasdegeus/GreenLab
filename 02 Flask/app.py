7629a91aacb22e43bc6285a44f2954a30e461266
TimpCode <55278273+TimpCode@users.noreply.github.com>
2021-10-29 14:18:45 +0200

    Add files via upload

---
+++ 02 Flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+# from AndroidRunner.Device import Device
+
+# noinspection PyUnusedLocal
+# def main(device: Device, *args: tuple, **kwargs: dict):
+#     pass
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='/home/pi/external_memory/android-runner/python2',
+            template_folder='/home/pi/external_memory/android-runner/python2')
+
+if __name__ == "__main__":
+    app.run(debug=True, host='192.168.0.243', port=5000)
+
+# import serve_webpage
